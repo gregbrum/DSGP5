89a5e5c584de0b61a7cb17eac42e6c5a265e1a05
Unknown <gregbrum@gmail.com>
2017-04-27 16:19:05 -0500

    Added main

--- DSGP5/DSGP5/Main.cpp
+++ DSGP5/DSGP5/Main.cpp
@@ -1,5 +1,19 @@
 #include "Graph.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-	WeightedGraph<int> w;
+	WeightedGraph<string> w;
+	w.addVertex("Houston");
+	w.addVertex("New_York");
+	w.addVertex("St_Louis");
+	w.addEdge("Houston", "New_York", 500);
+	w.addEdge("New_York", "St_Louis", 115);
+	w.addEdge("St_Louis", "Houston", 380);
+
+
+	system("pause");
+	return 0;
 }
